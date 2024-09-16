3454a04a4efcb8526d22e3d902365994d16a9ad0
Ethan Swan <eswan18@rocketmail.com>
2024-09-15 21:47:34 -0500

    Make landing page presentable.

--- forecast-app/app/page.tsx
+++ forecast-app/app/page.tsx
@@ -2,15 +2,14 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="flex flex-col">
-          <h1 className="text-lg">Links</h1>
-          <ul className="list-disc">
-            <li><Link href="/props/2024">2024 Props</Link></li>
-            <li><Link href="/scores/2024">2024 Scores</Link></li>
-          </ul>
-        </div>
+    <main className="flex min-h-screen flex-col items-center justify-start p-24">
+      <h1 className="text-lg my-3">Welcome!</h1>
+      <div className="flex flex-col items-start">
+        <p>You're probably looking for one of these links</p>
+        <ul className="list-disc px-4">
+          <li className="underline"><Link href="/props/2024">2024 Props</Link></li>
+          <li className="underline"><Link href="/scores/2024">2024 Scores</Link></li>
+        </ul>
       </div>
     </main>
   );
