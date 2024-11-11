82d91f2ec82b89c116fa60fd668fb5393ec8284e
Ethan Swan <eswan18@rocketmail.com>
2024-11-11 14:00:43 -0600

    Make landing page more similar to other pages.

--- forecast-app/app/page.tsx
+++ forecast-app/app/page.tsx
@@ -1,11 +1,12 @@
+import PageHeading from "@/components/page-heading";
 import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <h1 className="text-lg my-3">Welcome!</h1>
-      <div className="flex flex-col items-start">
-        <p>You&apos;re probably looking for one of these links</p>
+    <main className="flex flex-col items-center justify-between py-8 px-8 lg:py-12 lg:px-24">
+      <div className="w-full max-w-lg">
+        <PageHeading title="Welcome" />
+        Quick Links:
         <ul className="list-disc px-4">
           <li className="underline"><Link href="/props/2024">2024 Props</Link></li>
           <li className="underline"><Link href="/scores/2024">2024 Scores</Link></li>
