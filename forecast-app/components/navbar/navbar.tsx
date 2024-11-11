ee6e7ecd4ef16caf3c6e3b76192d16b79c463b0d
Ethan Swan <ethanpswan@gmail.com>
2024-11-11 07:45:13 -0600

    Fix linting issue.

--- forecast-app/components/navbar/navbar.tsx
+++ forecast-app/components/navbar/navbar.tsx
@@ -35,7 +35,7 @@ export default async function NavBar() {
         </Link>
         <NavigationMenuList>
           {links.map(({ href, label }) => (
-            <NavigationMenuItem>
+            <NavigationMenuItem key={href}>
               <NavigationMenuLink
                 href={href}
                 className={navigationMenuTriggerStyle()}
