a56fab47c166eccd932c6f49e6e711e2a23d29fe
Ethan Swan <ethanpswan@gmail.com>
2024-11-15 18:56:09 -0600

    Correct the replace_user script.

--- forecast-app/sql_scripts/replace_user.sql
+++ forecast-app/sql_scripts/replace_user.sql
@@ -1,6 +1,6 @@
 -- This script is for removing a legacy user record (which isn't linked to a login)
 -- and repointing all the associated forecasts to the new user record.
--- Replace all instances of '2' with the legacy user ID (to delete) and
--- all instances of '19' with the new user ID.
-update forecasts set user_id = 22 where user_id = 5;
-delete from users where id = 5;
+-- Replace all instances of '4' with the legacy user ID (to delete) and
+-- all instances of '23' with the new user ID.
+update forecasts set user_id = 23 where user_id = 4;
+delete from users where id = 4;
