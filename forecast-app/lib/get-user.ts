cc1d915b714bd2fd707967cbc6c5041566beaeba
Ethan Swan <ethanpswan@gmail.com>
2024-11-27 20:58:49 -0500

    Remove unneeded server action.

--- forecast-app/lib/get-user.ts
+++ forecast-app/lib/get-user.ts
@@ -1,20 +1,11 @@
 "use server";
 
-import { NextRequest } from 'next/server';
 import { cookies } from 'next/headers';
 import jwt from 'jsonwebtoken';
 import { db } from '@/lib/database';
 import { VUser } from '@/types/db_types';
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
-
-export async function getUserFromRequest(req: NextRequest): Promise<VUser | null> {
-  const token = req.cookies.get('token')?.value;
-  if (!token) {
-    return null;
-  }
-  return await getUserFromToken(token);
-}
 
 export async function getUserFromCookies(): Promise<VUser | null> {
   const token = (await cookies()).get('token')?.value;
