ba6419a68f6324e8eb27c0e42892dab53b5f683f
Ethan Swan <eswan18@rocketmail.com>
2024-11-27 22:22:48 -0500

    Support redirect on login (#21)
    * Get a redirect working for /forecasts/record.
    
    * Add redirects for other protected pages.

--- forecast-app/app/props/suggest/page.tsx
+++ forecast-app/app/props/suggest/page.tsx
@@ -1,13 +1,11 @@
 import PageHeading from "@/components/page-heading";
 import { SuggestPropForm } from "./suggest-prop-form";
 import { getUserFromCookies } from "@/lib/get-user";
-import { redirect } from "next/navigation";
+import { loginAndRedirect } from "@/lib/get-user";
 
 export default async function SuggestPropPage() {
   const user = await getUserFromCookies();
-  if (!user) {
-    redirect("/login");
-  }
+  if (!user) await loginAndRedirect({ url: "/props/suggest" });
   return (
     <main className="flex flex-col items-center justify-between py-8 px-8 lg:py-12 lg:px-24">
       <div className="w-full max-w-lg">
