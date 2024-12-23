e874377e3ee497c7ba84f63da2bec01473f88110
Ethan Swan <ethanpswan@gmail.com>
2024-12-22 22:48:51 -0600

    Add migration for uniq user/prop IDs in forecasts.

---
+++ forecast-app/migrations/1734929073136_alter-forecasts-table-make-uniq-constraint.ts
@@ -0,0 +1,13 @@
+import type { Kysely } from 'kysely'
+
+export async function up(db: Kysely<any>): Promise<void> {
+	await db.schema.alterTable('forecasts')
+		.addUniqueConstraint('prop_user_unique', ['prop_id', 'user_id'], (builder) => builder.nullsNotDistinct())
+		.execute()
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+	await db.schema.alterTable('forecasts')
+		.dropConstraint('prop_user_unique')
+		.execute()
+}
