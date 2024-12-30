8de04bfc330db88fe448cc47b2fdb50e6d917079
Ethan Swan <ethanpswan@gmail.com>
2024-12-30 10:46:36 -0600

    Remove two props.
    Jimmy Carter has died before the end of 2025, and
    there were two essentially-identical props about
    Jerome Powell remaining head of the Fed.

--- forecast-app/sql_scripts/delete_prop.sql
+++ forecast-app/sql_scripts/delete_prop.sql
@@ -1,6 +1,5 @@
 -- Michel Barnier was removed as PM of France before 2025 even began,
 -- so we need to remove the relevant prop from the database.
-
 DELETE FROM forecasts WHERE prop_id = 67;
 DELETE FROM props WHERE id = 67;
 
@@ -8,3 +7,12 @@ DELETE FROM props WHERE id = 67;
 -- so this is off the board.
 DELETE FROM forecasts WHERE prop_id = 64;
 DELETE FROM props WHERE id = 64;
+
+-- Jimmy Carter died before the end of 2024, so his 2025 prop is removed.
+DELETE FROM forecasts WHERE prop_id = 65;
+DELETE FROM props WHERE id = 65;
+
+-- There were two props about Jerome Powell still being in his position at the end of
+-- 2025 (47 & 70), so we need to remove the latter.
+DELETE FROM forecasts WHERE prop_id = 70;
+DELETE FROM props WHERE id = 70;
