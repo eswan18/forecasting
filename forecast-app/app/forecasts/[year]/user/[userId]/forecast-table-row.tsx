4d11e8ef17304a1477ef35df85bdbb08cf10510b
Ethan Swan <ethanpswan@gmail.com>
2025-03-20 17:02:09 -0500

    Move forecast table filters into search params.

---
+++ forecast-app/app/forecasts/[year]/user/[userId]/forecast-table-row.tsx
@@ -0,0 +1,66 @@
+import { Check, X } from "lucide-react";
+
+export type ScoredForecast = {
+  category_id: number;
+  category_name: string;
+  prop_id: number;
+  prop_text: string;
+  prop_notes: string | null;
+  resolution: boolean | null;
+  user_id: number;
+  forecast: number;
+  forecast_id: number;
+  penalty: number | null;
+  year: number;
+};
+
+export default function ForecastTableRow(
+  { row, editable }: { row: ScoredForecast; editable: boolean },
+) {
+  const resolution = row.resolution !== null
+    ? (
+      row.resolution
+        ? (
+          <Check
+            size={20}
+            strokeWidth={3}
+            className="-translate-y-1.5 text-green-500"
+          />
+        )
+        : (
+          <X
+            size={20}
+            strokeWidth={3}
+            className="-translate-y-1.5 text-destructive"
+          />
+        )
+    )
+    : <p className="text-muted-foreground">?</p>;
+  const penaltyString = row.penalty !== null
+    ? <p>{row.penalty.toFixed(2)}</p>
+    : <p className="text-muted-foreground">?</p>;
+  return (
+    <div className="w-full bg-card grid grid-cols-[1fr_1fr] sm:grid-cols-[2fr_1fr] px-4 py-4 border gap-x-1">
+      <div className="flex flex-col gap-y-1">
+        <p>{row.prop_text}</p>
+        <p className="text-muted-foreground text-sm">{row.category_name}</p>
+      </div>
+      <div className="grid grid-cols-[1fr_1fr_1fr] gap-x-1 text-right">
+        <div className="w-full flex flex-row items-end justify-center sm:justify-end text-lg font-bold">
+          {resolution}
+        </div>
+        <div className="w-full flex flex-row items-end justify-end text-lg font-bold">
+          {row.forecast.toFixed(2)}
+        </div>
+        <div className="w-full flex flex-row items-end justify-end text-lg font-bold">
+          {penaltyString}
+        </div>
+        <p className="text-xs text-muted-foreground">
+          <span className="hidden sm:inline">Resolution</span>
+        </p>
+        <p className="text-xs text-muted-foreground">Predicted</p>
+        <p className="text-xs text-muted-foreground">Penalty</p>
+      </div>
+    </div>
+  );
+}
--- forecast-app/app/forecasts/[year]/user/[userId]/forecast-table.tsx
+++ forecast-app/app/forecasts/[year]/user/[userId]/forecast-table.tsx
@@ -1,31 +1,14 @@
 "use client";
 
-import { Dispatch, SetStateAction, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { ArrowDown, ArrowUp, ArrowUpDown, Check, X } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Checkbox } from "@/components/ui/checkbox";
 
-export type ScoredForecast = {
-  category_id: number;
-  category_name: string;
-  prop_id: number;
-  prop_text: string;
-  prop_notes: string | null;
-  resolution: boolean | null;
-  user_id: number;
-  forecast: number;
-  forecast_id: number;
-  penalty: number | null;
-  year: number;
-};
+import ForecastTableHeader from "./forecast-table-header";
+import ForecastTableRow, { ScoredForecast } from "./forecast-table-row";
 
-interface SortStatus {
-  column: string;
-  direction: "asc" | "desc";
-}
-
-interface FilterStatus {
+interface ForecastTableSearchParams {
+  sortColumn: string;
+  sortAsc: boolean;
   resolution: (boolean | null)[];
 }
 
@@ -37,19 +20,66 @@ interface ForecastTableProps {
 export default function ForecastTable(
   { data, editable }: ForecastTableProps,
 ) {
-  const [sortStatus, setSortStatus] = useState<SortStatus | null>(null);
-  const [filter, setFilter] = useState<FilterStatus>({
-    resolution: [true, false],
-  });
-  // Filters
-  if (filter.resolution.length !== 3) {
-    data = data.filter((row) => filter.resolution.includes(row.resolution));
+  const router = useRouter();
+  const pathName = usePathname();
+  const rawSearchParams = useSearchParams();
+  const searchParams: ForecastTableSearchParams = {
+    sortColumn: rawSearchParams.get("sortColumn") || "forecast",
+    sortAsc: rawSearchParams.get("sortAsc") === "true",
+    resolution: rawSearchParams.getAll("resolution").map((value) => {
+      if (value === "true") return true;
+      else if (value === "false") return false;
+      else return null;
+    }),
+  };
+  if (searchParams.resolution.length === 0) {
+    // If no resolution filters are set, that means we use the default: show only
+    // resolved props, which are [true, false],
+    searchParams.resolution = [true, false];
+  }
+  const setResolutionFilter = (filter: (boolean | null)[]) => {
+    searchParams.resolution = filter;
+    updateSearchParams(searchParams);
+  };
+  const setSort = (column: string, asc: boolean) => {
+    searchParams.sortColumn = column;
+    searchParams.sortAsc = asc;
+    updateSearchParams(searchParams);
+  };
+  const updateSearchParams = (params: ForecastTableSearchParams) => {
+    const searchParams = new URLSearchParams();
+    if (params.sortColumn && params.sortColumn !== "forecast") {
+      searchParams.set("sortColumn", params.sortColumn);
+    }
+    if (params.sortAsc) searchParams.set("sortAsc", "true");
+    if (
+      !(params.resolution.includes(true) && params.resolution.includes(false) &&
+        !params.resolution.includes(null))
+    ) {
+      // Only add resolution filter if it's not the default
+      params.resolution.forEach((value) => {
+        if (value === null) {
+          searchParams.append("resolution", "null");
+        } else {
+          searchParams.append("resolution", value.toString());
+        }
+      });
+    }
+    router.push(`${pathName}?${searchParams.toString()}`);
+  };
+  // Filter
+  if (searchParams.resolution.length !== 0) {
+    data = data.filter((row) =>
+      searchParams.resolution.includes(row.resolution)
+    );
   }
   // Sort
-  if (sortStatus !== null) {
+  console.log(searchParams.sortColumn);
+  console.log(searchParams.sortAsc);
+  if (searchParams.sortColumn !== null) {
     data = data.sort((a, b) => {
-      if (sortStatus?.direction === "asc") {
-        switch (sortStatus.column) {
+      if (searchParams.sortAsc) {
+        switch (searchParams.sortColumn) {
           case "forecast":
             return a.forecast > b.forecast ? 1 : -1;
           case "resolution":
@@ -65,8 +95,8 @@ export default function ForecastTable(
           default:
             return 0;
         }
-      } else if (sortStatus?.direction === "desc") {
-        switch (sortStatus.column) {
+      } else {
+        switch (searchParams.sortColumn) {
           case "forecast":
             return a.forecast < b.forecast ? 1 : -1;
           case "resolution":
@@ -83,15 +113,18 @@ export default function ForecastTable(
             return 0;
         }
       }
-      return 0;
     });
   }
   return (
     <div className="w-full">
-      <ForecastTableFilterPanel filter={filter} setFilter={setFilter} />
+      <ForecastTableFilterPanel
+        resolutionFilter={searchParams.resolution}
+        setResolutionFilter={setResolutionFilter}
+      />
       <ForecastTableHeader
-        sortStatus={sortStatus}
-        setSortStatus={setSortStatus}
+        sortColumn={searchParams.sortColumn ?? "forecast"}
+        sortAsc={searchParams.sortAsc ?? true}
+        setSort={setSort}
       />
       <ul className="w-full flex flex-col">
         {data.map((row) => (
@@ -104,147 +137,34 @@ export default function ForecastTable(
   );
 }
 
-function ForecastTableFilterPanel({ filter, setFilter }: {
-  filter: FilterStatus;
-  setFilter: Dispatch<SetStateAction<FilterStatus>>;
-}) {
+function ForecastTableFilterPanel(
+  { resolutionFilter, setResolutionFilter }: {
+    resolutionFilter: (boolean | null)[];
+    setResolutionFilter: (filter: (boolean | null)[]) => void;
+  },
+) {
   const handleCheck = (checked: boolean) => {
     if (checked) {
-      setFilter({ resolution: [true, false] });
+      if (resolutionFilter.length === 0) {
+        setResolutionFilter([true, false]);
+      } else {
+        // Remove null from the filter
+        setResolutionFilter(resolutionFilter.filter((value) => value !== null));
+      }
     } else {
-      setFilter({ resolution: [true, false, null] });
+      // Add null to the filter
+      if (!resolutionFilter.includes(null)) {
+        setResolutionFilter([...resolutionFilter, null]);
+      }
     }
   };
   return (
     <div className="w-full flex flex-row justify-center sm:justify-start px-2.5 items-center gap-x-2 text-muted-foreground mb-4 sm:mb-2">
       <p>Hide unresolved props</p>
       <Checkbox
-        checked={!filter.resolution.includes(null)}
+        checked={!resolutionFilter.includes(null)}
         onCheckedChange={handleCheck}
       />
-    </div>
-  );
-}
-
-function ForecastTableHeader(
-  { sortStatus, setSortStatus }: {
-    sortStatus: SortStatus | null;
-    setSortStatus: Dispatch<SetStateAction<SortStatus | null>>;
-  },
-) {
-  const handleSortClick = (column: string) => {
-    setSortStatus((prev) => {
-      if (prev && prev.column === column) {
-        return {
-          column,
-          direction: prev.direction === "asc" ? "desc" : "asc",
-        };
-      } else {
-        return { column, direction: "desc" };
-      }
-    });
-  };
-  return (
-    <div className="w-full grid grid-cols-[1fr_1fr] sm:grid-cols-[2fr_1fr] px-4 pb-1 border-b text-muted-foreground">
-      <div></div>
-      <div className="col-span-1 grid grid-cols-[1fr_1fr_1fr] gap-x-1 text-right">
-        <SortColumnHeader
-          onClick={() => handleSortClick("resolution")}
-          selection={sortStatus?.column === "resolution"
-            ? sortStatus.direction
-            : undefined}
-          className="justify-center sm:justify-end"
-        />
-        <SortColumnHeader
-          onClick={() => handleSortClick("forecast")}
-          selection={sortStatus?.column === "forecast"
-            ? sortStatus.direction
-            : undefined}
-        />
-        <SortColumnHeader
-          onClick={() => handleSortClick("penalty")}
-          selection={sortStatus?.column === "penalty"
-            ? sortStatus.direction
-            : undefined}
-        />
-      </div>
-    </div>
-  );
-}
-
-function SortColumnHeader(
-  { onClick, selection, className }: {
-    onClick: () => void;
-    selection: "asc" | "desc" | undefined;
-    className?: string;
-  },
-) {
-  className = cn("w-full flex flex-row items-end justify-end", className);
-  return (
-    <div className={className}>
-      <Button
-        variant="ghost"
-        className="h-6 p-0 flex flex-row gap-x-1 m-0"
-        onClick={onClick}
-      >
-        <span className="hidden sm:inline">Sort</span>
-        {selection === undefined
-          ? <ArrowUpDown size={20} />
-          : selection === "asc"
-          ? <ArrowUp size={20} />
-          : <ArrowDown size={20} />}
-      </Button>
-    </div>
-  );
-}
-
-function ForecastTableRow(
-  { row, editable }: { row: ScoredForecast; editable: boolean },
-) {
-  const resolution = row.resolution !== null
-    ? (
-      row.resolution
-        ? (
-          <Check
-            size={20}
-            strokeWidth={3}
-            className="-translate-y-1.5 text-green-500"
-          />
-        )
-        : (
-          <X
-            size={20}
-            strokeWidth={3}
-            className="-translate-y-1.5 text-destructive"
-          />
-        )
-    )
-    : <p className="text-muted-foreground">?</p>;
-  const penaltyString = row.penalty !== null
-    ? <p>{row.penalty.toFixed(2)}</p>
-    : <p className="text-muted-foreground">?</p>;
-  return (
-    <div className="w-full bg-card grid grid-cols-[1fr_1fr] sm:grid-cols-[2fr_1fr] px-4 py-4 border gap-x-1">
-      <div className="flex flex-col gap-y-1">
-        <p>{row.prop_text}</p>
-        <p className="text-muted-foreground text-sm">{row.category_name}</p>
-      </div>
-      <div className="grid grid-cols-[1fr_1fr_1fr] gap-x-1 text-right">
-        <div className="w-full flex flex-row items-end justify-center sm:justify-end text-lg font-bold">
-          {resolution}
-        </div>
-        <div className="w-full flex flex-row items-end justify-end text-lg font-bold">
-          {row.forecast.toFixed(2)}
-        </div>
-        <div className="w-full flex flex-row items-end justify-end text-lg font-bold">
-          {penaltyString}
-        </div>
-        <p className="text-xs text-muted-foreground">
-          <span className="hidden sm:inline">Resolution</span>
-        </p>
-        <p className="text-xs text-muted-foreground">Predicted</p>
-        <p className="text-xs text-muted-foreground">Penalty</p>
-      </div>
     </div>
   );
 }
