674e9cfdb10dbcd92642c828b35cc197b89f668a
Ethan Swan <ethanpswan@gmail.com>
2025-03-25 17:34:46 -0500

    Create Update Resolution Dialog.

--- components/tables/prop-table/resolution-select-widget.tsx
+++ components/tables/prop-table/resolution-select-widget.tsx
@@ -1,4 +1,16 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Check, X } from "lucide-react";
+import { useState } from "react";
 
 export default function ResolutionSelectWidget(
   { resolution, setResolution }: {
@@ -6,41 +18,94 @@ export default function ResolutionSelectWidget(
     setResolution: (value: boolean | undefined) => void;
   },
 ) {
+  const [lastClicked, setLastClicked] = useState<boolean | undefined>(
+    undefined,
+  );
+  const [resolutionNotes, setResolutionNotes] = useState<string>("");
+  const [dialogIsOpen, setDialogIsOpen] = useState<boolean>(false);
+  // If the last clicked button is the same as the current resolution, we want to
+  // unresolve it. Otherwise, we want to set it to the last clicked value.
+  const resolutionToSet = lastClicked === resolution ? undefined : lastClicked;
   const buttonClasses = (selected: boolean) =>
     `px-4 py-1.5 rounded-full transition-colors duration-200 hover:bg-accent hover:text-accent-foreground ${
       selected ? "bg-background" : "text-muted-foreground"
     }`;
-  const handleTrueClick = () => {
-    // If already true, unresolve the prop. Otherwise resolve it to true.
-    resolution === true ? setResolution(undefined) : setResolution(true);
-  };
-  const handleFalseClick = () => {
-    // If already false, unresolve the prop. Otherwise resolve it to false.
-    resolution === false ? setResolution(undefined) : setResolution(false);
-  };
   return (
     <div className="inline-flex justify-between items-center bg-secondary rounded-full p-1 w-fit">
-      <button
-        className={buttonClasses(resolution === false)}
-        onClick={handleFalseClick}
-      >
-        <X
-          size={20}
-          strokeWidth={3}
-          className={resolution === false ? "text-destructive" : ""}
-        />
-      </button>
-
-      <button
-        className={buttonClasses(resolution === true)}
-        onClick={handleTrueClick}
-      >
-        <Check
-          size={20}
-          strokeWidth={3}
-          className={resolution === true ? "text-green-500" : ""}
-        />
-      </button>
+      <Dialog open={dialogIsOpen} onOpenChange={setDialogIsOpen}>
+        <DialogTrigger asChild>
+          <button
+            className={buttonClasses(resolution === false)}
+            onClick={() => setLastClicked(false)}
+          >
+            <X
+              size={20}
+              strokeWidth={3}
+              className={resolution === false ? "text-destructive" : ""}
+            />
+          </button>
+        </DialogTrigger>
+        <DialogTrigger asChild>
+          <button
+            className={buttonClasses(resolution === true)}
+            onClick={() => setLastClicked(true)}
+          >
+            <Check
+              size={20}
+              strokeWidth={3}
+              className={resolution === true ? "text-green-500" : ""}
+            />
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              {resolutionToSet === undefined
+                ? "Unresolve Prop"
+                : "Resolve Prop"}
+            </DialogTitle>
+          </DialogHeader>
+          {resolutionToSet !== undefined
+            ? (
+              <div>
+                <Label>
+                  Add a note (optional)
+                </Label>
+                <Input
+                  type="text"
+                  placeholder="Details about why this was resolved."
+                  value={resolutionNotes}
+                  onChange={(e) => setResolutionNotes(e.target.value)}
+                />
+              </div>
+            )
+            : <p>Are you sure?</p>}
+          <DialogFooter>
+            {resolutionToSet === undefined
+              ? (
+                <Button
+                  variant="destructive"
+                  onClick={() => {
+                    setResolution(undefined);
+                    setDialogIsOpen(false);
+                  }}
+                >
+                  Unresolve
+                </Button>
+              )
+              : (
+                <Button
+                  onClick={() => {
+                    setResolution(resolutionToSet);
+                    setDialogIsOpen(false);
+                  }}
+                >
+                  Resolve to {resolutionToSet ? "True" : "False"}
+                </Button>
+              )}
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
