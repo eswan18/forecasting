6d45f361cb78ba41177a138e73587592a45b56b9
Ethan Swan <ethanpswan@gmail.com>
2025-03-26 05:56:47 -0500

    Add migration for user_id column.

--- migrations/1742986338172_alter-props-table-add-user-id-column.ts
+++ migrations/1742986338172_alter-props-table-add-user-id-column.ts
@@ -1,15 +1,15 @@
 import type { Kysely } from 'kysely'
 
 export async function up(db: Kysely<any>): Promise<void> {
-	await db
-		.insertInto('feature_flags')
-		.values({ name: 'personal-props', user_id: 1, enabled: true })
+	await db.schema
+		.alterTable('props')
+		.addColumn('user_id', 'integer', (col) => col.references('users.id'))
 		.execute()
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
-	await db
-		.deleteFrom('feature_flags')
-		.where('name', '=', 'personal-props')
+	await db.schema
+		.alterTable('props')
+		.dropColumn('user_id')
 		.execute()
 }
