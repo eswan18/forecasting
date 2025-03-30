f85f0d3cdbce33b69534f39e2733513b5aeec360
Ethan Swan <ethanpswan@gmail.com>
2025-03-30 12:18:29 -0500

    Fix competition props page.

--- app/competitions/[competitionId]/page.tsx
+++ app/competitions/[competitionId]/page.tsx
@@ -1,7 +1,7 @@
 import { getCompetitions, getProps } from "@/lib/db_actions";
 import PropTable from "@/components/tables/prop-table";
 import PageHeading from "@/components/page-heading";
-import YearSelector from "./year-selector";
+import CompetitionSelector from "./competition-selector";
 import { getUserFromCookies } from "@/lib/get-user";
 import ErrorPage from "@/components/pages/error-page";
 
@@ -19,6 +19,10 @@ export default async function Page(
   const allowEdits = user?.is_admin || false;
   // Passing null as the userId gets us only public props.
   const propsAndResolutions = await getProps({ competitionId, userId: null });
+  async function makeRedirectLink(id: number) {
+    "use server";
+    return `/competitions/${id}`;
+  }
   return (
     <main className="flex flex-col items-center justify-between py-8 px-8 lg:py-12 lg:px-24">
       <div className="w-full max-w-3xl">
@@ -26,7 +30,11 @@ export default async function Page(
           title={`${thisCompetition.name}: Props`}
           className="flex flex-row flex-wrap gap-x-4 lg:gap-x-8 items-end mb-4 sm:mb-8"
         >
-          <YearSelector years={[123]} selectedYear={123} />
+          <CompetitionSelector
+            competitions={competitions}
+            selectedCompetitionId={competitionId}
+            redirectOnSelect={makeRedirectLink}
+          />
         </PageHeading>
         <PropTable data={propsAndResolutions} editable={allowEdits} />
       </div>
