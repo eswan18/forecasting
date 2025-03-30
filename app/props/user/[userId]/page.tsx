95438d48b7ffcb54a5fa1a0f135f82fb93f5820c
Ethan Swan <ethanpswan@gmail.com>
2025-03-30 17:28:16 -0500

    Fix personal props page.

--- app/props/user/[userId]/page.tsx
+++ app/props/user/[userId]/page.tsx
@@ -1,19 +1,13 @@
 import { getProps } from "@/lib/db_actions";
 import PropTable from "@/components/tables/prop-table";
 import PageHeading from "@/components/page-heading";
-import YearSelector from "../../year-selector";
 import { getUserFromCookies } from "@/lib/get-user";
 import ErrorPage from "@/components/pages/error-page";
 
 export default async function Page(
-  { params }: { params: Promise<{ year: string; userId: string }> },
+  { params }: { params: Promise<{ userId: string }> },
 ) {
-  const year = parseInt((await params).year, 10);
   const userId = parseInt((await params).userId, 10);
-  // Check that year & user were numbers
-  if (isNaN(year)) {
-    throw new Error("Invalid year");
-  }
   if (isNaN(userId)) {
     throw new Error("Invalid user ID");
   }
@@ -24,8 +18,8 @@ export default async function Page(
         <p>You don&apos;t have permission to view props for other users.</p>
         <p className="text-muted-foreground">
           Your user ID is{" "}
-          <code className="bg-muted">{user?.id}</code>, but you&apos;re attempting to
-          view props for user ID{" "}
+          <code className="bg-muted">{user?.id}</code>, but you&apos;re
+          attempting to view props for user ID{" "}
           <code>{userId}</code>.<br />Please log in with the correct account.
         </p>
       </ErrorPage>
@@ -33,7 +27,7 @@ export default async function Page(
   }
   const years = [1, 2, 3];
   years.sort((a, b) => b - a);
-  const propsAndResolutions = await getProps({ year, userId: userId });
+  const propsAndResolutions = await getProps({ userId: userId });
   return (
     <main className="flex flex-col items-center justify-between py-8 px-8 lg:py-12 lg:px-24">
       <div className="w-full max-w-3xl">
@@ -41,10 +35,6 @@ export default async function Page(
           title="Personal Props"
           className="flex flex-row flex-wrap gap-x-4 lg:gap-x-8 items-end mb-4 sm:mb-8"
         >
-          <YearSelector
-            years={years}
-            selectedYear={year}
-          />
         </PageHeading>
         <PropTable
           data={propsAndResolutions}
