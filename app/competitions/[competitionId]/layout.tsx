2415b6fa623d497b4e9ae82b824a82cf1832beff
Ethan Swan <ethanpswan@gmail.com>
2025-04-02 10:11:45 -0500

    Add a better way to switch between competitions.

--- app/competitions/[competitionId]/layout.tsx
+++ app/competitions/[competitionId]/layout.tsx
@@ -1,10 +1,20 @@
 import PageHeading from "@/components/page-heading";
 import ErrorPage from "@/components/pages/error-page";
-import { getCompetitionById, getCompetitions } from "@/lib/db_actions";
+import { getCompetitions } from "@/lib/db_actions";
 import CompetitionSelector from "@/components/selectors/competition-selector";
-import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import CompetitionTabs from "./competition-tabs";
+import { ChevronDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 export default async function CompetitionLayout({
   children,
@@ -16,10 +26,14 @@ export default async function CompetitionLayout({
   const { competitionId: competitionIdString } = await params;
   const competitionId = parseInt(competitionIdString, 10);
   if (isNaN(competitionId)) {
-    return <ErrorPage title={`Invalid competition ID '${competitionIdString}'`} />;
+    return (
+      <ErrorPage title={`Invalid competition ID '${competitionIdString}'`} />
+    );
   }
-  const competition = await getCompetitionById(competitionId);
   const competitions = await getCompetitions();
+  const competition = competitions.find(
+    (competition) => competition.id === competitionId,
+  );
   if (!competition) {
     return <ErrorPage title="Competition not found" />;
   }
@@ -32,16 +46,49 @@ export default async function CompetitionLayout({
   }
   return (
     <main className="flex flex-col items-center justify-between py-8 px-8 lg:py-12 lg:px-24">
-      <PageHeading title={competition.name}>
-        <div className="flex flex-row items-center gap-2 mt-2">
-          <Label>Other Competitions</Label>
-          <CompetitionSelector
-            competitions={competitions}
-            selectedCompetitionId={competitionId}
-            redirectOnSelect={makeRedirectLink}
-          />
-        </div>
+      <PageHeading
+        title={competition.name}
+        className="flex flex-row items-start"
+      >
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>All Competitions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            {competitions.map((competition) => (
+              competitionId === competition.id
+              ? (
+                <DropdownMenuItem disabled key={competition.id}>
+                  {competition.name}
+                </DropdownMenuItem>
+              )
+              : (
+              <Link
+                href={`/competitions/${competition.id}/forecasts`}
+                key={competition.id}
+              >
+                <DropdownMenuItem>
+                  {competition.name}
+                </DropdownMenuItem>
+              </Link>
+            )))}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </PageHeading>
+      <div className="flex flex-col items-center justify-start gap-y-2 mb-4">
+        <p>
+          <span className="text-muted-foreground">Started</span>{" "}
+          {String(competition.forecasts_due_date)}
+        </p>
+        <p>
+          <span className="text-muted-foreground">Ends</span>{" "}
+          {String(competition.end_date)}
+        </p>
+      </div>
       <div className="w-full max-w-3xl">
         <div className="flex flex-col items-center justify-start mb-4 gap-y-2">
           <Separator />
