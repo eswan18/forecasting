82cd8d327dd36a2892de37825af2ee26fbf13f5d
Ethan Swan <ethanpswan@gmail.com>
2025-04-01 22:39:38 -0500

    Fix some typing issues.

--- lib/db_actions/categories.ts
+++ lib/db_actions/categories.ts
@@ -1,8 +1,9 @@
 "use server";
+import { Category } from "@/types/db_types";
 import { getUserFromCookies } from "../get-user";
 import { db } from '@/lib/database';
 
-export async function getCategories(): Promise<{ id: number, name: string }[]> {
+export async function getCategories(): Promise<Category[]> {
   const currentUser = await getUserFromCookies();
   if (!currentUser) {
     throw new Error('Unauthorized');
