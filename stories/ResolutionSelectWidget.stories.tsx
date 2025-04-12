b5bed5c41f4701b21e9340141c08b02bed97df20
Ethan Swan <ethanpswan@gmail.com>
2025-04-12 14:08:15 -0500

    Add a couple of stories that work.

---
+++ stories/ResolutionSelectWidget.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ResolutionSelectWidget from "@/components/resolution-select-widget";
+
+const meta = {
+  title: "Example/ResolutionSelectWidget",
+  component: ResolutionSelectWidget,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof ResolutionSelectWidget>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const ResolvedTrue: Story = {
+  args: {
+    resolution: true,
+    setResolution: () => {},
+  },
+};
+
+export const ResolvedFalse: Story = {
+  args: {
+    resolution: false,
+    setResolution: () => {},
+  },
+};
+
+export const Unresolved: Story = {
+  args: {
+    resolution: undefined,
+    setResolution: () => {},
+  },
+};
+
+export const Small: Story = {
+  args: {
+    resolution: true,
+    setResolution: () => {},
+    size: "sm",
+  },
+};
