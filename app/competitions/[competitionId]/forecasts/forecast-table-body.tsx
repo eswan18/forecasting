3ced9a2647c9857031ef43f756c2862f5f02d603
Ethan Swan <ethanpswan@gmail.com>
2025-05-11 21:59:08 -0500

    Manage which forecasts are editable.

--- app/competitions/[competitionId]/forecasts/forecast-table-body.tsx
+++ app/competitions/[competitionId]/forecasts/forecast-table-body.tsx
@@ -2,6 +2,8 @@
 
 import { getForecasts, VForecastsOrderByExpression } from "@/lib/db_actions";
 import ForecastTableRow from "./forecast-table-row";
+import { getUserFromCookies } from "@/lib/get-user";
+import { VForecast } from "@/types/db_types";
 
 export interface ForecastTableBodyProps {
   competitionId: number;
@@ -16,6 +18,7 @@ export default async function ForecastTableBody(
   { competitionId, userId, resolution, sortColumn, sortAsc, propText }:
     ForecastTableBodyProps,
 ) {
+  const currentUser = await getUserFromCookies();
   let data = await getForecasts({
     userId,
     competitionId,
@@ -31,11 +34,25 @@ export default async function ForecastTableBody(
       row.prop_text?.toLowerCase().includes(propText.toLowerCase())
     );
   }
+  const isRowEditable = (row: VForecast) => {
+    if (row.user_id === currentUser?.id) {
+      if (
+        row.competition_forecasts_due_date === null ||
+        new Date() < row.competition_forecasts_due_date
+      ) {
+        return true;
+      }
+    }
+    if (currentUser?.is_admin) {
+      return true;
+    }
+    return false;
+  };
   return (
     <ul className="w-full flex flex-col">
       {data.map((row) => (
         <li key={row.forecast_id}>
-          <ForecastTableRow row={row} editable={false} />
+          <ForecastTableRow row={row} editable={isRowEditable(row)} />
         </li>
       ))}
     </ul>
