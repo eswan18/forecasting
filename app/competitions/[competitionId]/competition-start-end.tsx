ad3b19c97bebfab2bfea243d657fc6bbc451e006
Ethan Swan <ethanpswan@gmail.com>
2025-11-23 19:18:23 -0600

    Fix competition date display logic.

--- app/competitions/[competitionId]/competition-start-end.tsx
+++ app/competitions/[competitionId]/competition-start-end.tsx
@@ -14,18 +14,68 @@ export default function CompetitionStartEnd({
   const status = getCompetitionStatusFromObject(competition);
   return (
     <div className="flex flex-row flex-wrap items-center justify-start gap-x-4 sm:gap-x-8 gap-y-2 mb-4 text-sm">
-      <p>
-        <span className="text-muted-foreground">
-          Forecasts {status === "upcoming" ? "close" : "closed"}
-        </span>{" "}
-        {formatInTimeZone(competition.forecasts_close_date, "UTC", DATE_FORMAT)}
-      </p>
-      <p>
-        <span className="text-muted-foreground">
-          {status === "ended" ? "Ended" : "Ends"}
-        </span>{" "}
-        {formatInTimeZone(competition.end_date, "UTC", DATE_FORMAT)}
-      </p>
+      {status === "upcoming" && (
+        <>
+          <p>
+            <span className="text-muted-foreground">Forecasts open</span>{" "}
+            {formatInTimeZone(
+              competition.forecasts_open_date,
+              "UTC",
+              DATE_FORMAT,
+            )}
+          </p>
+          <p>
+            <span className="text-muted-foreground">Forecasts close</span>{" "}
+            {formatInTimeZone(
+              competition.forecasts_close_date,
+              "UTC",
+              DATE_FORMAT,
+            )}
+          </p>
+        </>
+      )}
+      {status === "forecasts-open" && (
+        <>
+          <p>
+            <span className="text-muted-foreground">Forecasts opened</span>{" "}
+            {formatInTimeZone(
+              competition.forecasts_open_date,
+              "UTC",
+              DATE_FORMAT,
+            )}
+          </p>
+          <p>
+            <span className="text-muted-foreground">Forecasts close</span>{" "}
+            {formatInTimeZone(
+              competition.forecasts_close_date,
+              "UTC",
+              DATE_FORMAT,
+            )}
+          </p>
+        </>
+      )}
+      {status === "forecasts-closed" && (
+        <>
+          <p>
+            <span className="text-muted-foreground">Forecasts closed</span>{" "}
+            {formatInTimeZone(
+              competition.forecasts_close_date,
+              "UTC",
+              DATE_FORMAT,
+            )}
+          </p>
+          <p>
+            <span className="text-muted-foreground">Competition Ends</span>{" "}
+            {formatInTimeZone(competition.end_date, "UTC", DATE_FORMAT)}
+          </p>
+        </>
+      )}
+      {status === "ended" && (
+        <p>
+          <span className="text-muted-foreground">Ended</span>{" "}
+          {formatInTimeZone(competition.end_date, "UTC", DATE_FORMAT)}
+        </p>
+      )}
     </div>
   );
 }
